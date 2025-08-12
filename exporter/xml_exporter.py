6e7b13c0854a2e912ac01d0ab1e14406e466a264
Avtex19 <atukaberadze19@gmail.com>
2025-08-12 12:50:49 +0400

    feat: implement XmlExporter using dicttoxml for Room and Student data

---
+++ exporter/xml_exporter.py
@@ -0,0 +1,16 @@
+from typing import List
+from dataclasses import asdict
+from dicttoxml import dicttoxml
+
+from exporter.base import Exporter
+from models import Room
+
+
+class XmlExporter(Exporter):
+    def export(self, data: List[Room], file_path: str) -> None:
+        data_dicts = [asdict(room) for room in data]
+
+        xml_bytes = dicttoxml(data_dicts, custom_root="rooms", attr_type=False)
+
+        with open(file_path, "wb") as file:
+            file.write(xml_bytes)
